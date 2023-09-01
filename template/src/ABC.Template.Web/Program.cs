ba40663bffc185f4b54be0506207f1c7fca31546
witskeeper <witskeeper@gmail.com>
2023-09-02 00:25:42 +0800

    添加日志

--- template/src/ABC.Template.Web/Program.cs
+++ template/src/ABC.Template.Web/Program.cs
@@ -1,132 +1,174 @@
 using NetCorePal.Extensions.Primitives;
-using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using Prometheus;
 using System.Reflection;
 using Microsoft.AspNetCore.DataProtection;
 using StackExchange.Redis;
-using System;
 using FluentValidation.AspNetCore;
 using FluentValidation;
 using NetCorePal.Extensions.Domain.Json;
 using ABC.Template.Web.Application.Queries;
 using ABC.Template.Web.Application.IntegrationEventHandlers;
-using NetCorePal.Extensions.Repository;
 using ABC.Template.Web.Extensions;
-using NetCorePal.Extensions.DependencyInjection;
+using Serilog;
+using Serilog.Formatting.Json;
 
-var builder = WebApplication.CreateBuilder(args);
-#region SignalR
-builder.Services.AddHealthChecks();
-builder.Services.AddMvc();
-builder.Services.AddSignalR();
-#endregion
+Log.Logger = new LoggerConfiguration()
+    .Enrich.WithClientIp()
+    .WriteTo.Console(new JsonFormatter())
+    .CreateLogger();
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
+    builder.Host.UseSerilog();
 
-#region Prometheus监控
-builder.Services.AddHealthChecks().ForwardToPrometheus();
-builder.Services.AddHttpClient(Options.DefaultName)
+    #region SignalR
+
+    builder.Services.AddHealthChecks();
+    builder.Services.AddMvc();
+    builder.Services.AddSignalR();
+
+    #endregion
+
+    #region Prometheus监控
+
+    builder.Services.AddHealthChecks().ForwardToPrometheus();
+    builder.Services.AddHttpClient(Options.DefaultName)
         .UseHttpClientMetrics();
-#endregion
-// Add services to the container.
 
-#region  文件系统
-//TODO: 注册文件服务为fileprovider，如阿里云对象存储
+    #endregion
 
-#endregion
-#region 身份认证
-var redis = ConnectionMultiplexer.Connect(builder.Configuration.GetConnectionString("Redis")!);
-builder.Services.AddSingleton<IConnectionMultiplexer>(p => redis);
-builder.Services.AddDataProtection()
-    .PersistKeysToStackExchangeRedis(redis, "DataProtection-Keys");
-//.PersistKeysToFileSystem(new System.IO.DirectoryInfo("d://DataProtection-Keys"));
-#endregion
+    // Add services to the container.
 
-#region Controller
-builder.Services.AddControllers().AddJsonOptions(options =>
-{
-    options.JsonSerializerOptions.Converters.Add(new EntityIdJsonConverterFactory());
-});
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c => c.AddEntityIdSchemaMap()); //强类型id swagger schema 映射
-#endregion
+    #region 文件系统
 
-#region 公共服务
-builder.Services.AddSingleton<IClock, SystemClock>();
-#endregion
+    //TODO: 注册文件服务为fileprovider，如阿里云对象存储
 
+    #endregion
 
+    #region 身份认证
 
+    var redis = ConnectionMultiplexer.Connect(builder.Configuration.GetConnectionString("Redis")!);
+    builder.Services.AddSingleton<IConnectionMultiplexer>(p => redis);
+    builder.Services.AddDataProtection()
+        .PersistKeysToStackExchangeRedis(redis, "DataProtection-Keys");
+    //.PersistKeysToFileSystem(new System.IO.DirectoryInfo("d://DataProtection-Keys"));
 
-#region 集成事件
-builder.Services.AddTransient<OrderPaidIntegrationEventHandler>();
-#endregion
+    #endregion
 
-#region 模型验证器
-builder.Services.AddFluentValidationAutoValidation();
-builder.Services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
-#endregion
+    #region Controller
+
+    builder.Services.AddControllers().AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(new EntityIdJsonConverterFactory());
+    });
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen(c => c.AddEntityIdSchemaMap()); //强类型id swagger schema 映射
+
+    #endregion
+
+    #region 公共服务
+
+    builder.Services.AddSingleton<IClock, SystemClock>();
+
+    #endregion
 
 
-#region Mapper Provider
-builder.Services.AddMapperPrivider(Assembly.GetExecutingAssembly());
-#endregion
+    #region 集成事件
 
-#region Query
-builder.Services.AddScoped<OrderQuery>();
-#endregion
+    builder.Services.AddTransient<OrderPaidIntegrationEventHandler>();
+
+    #endregion
+
+    #region 模型验证器
+
+    builder.Services.AddFluentValidationAutoValidation();
+    builder.Services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+    #endregion
 
 
-#region 基础设施
-builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly()).AddUnitOfWorkBehaviors());
-builder.Services.AddRepositories(typeof(ApplicationDbContext).Assembly);
+    #region Mapper Provider
 
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-{
-    //options.UseInMemoryDatabase("ApplicationDbContext");
+    builder.Services.AddMapperPrivider(Assembly.GetExecutingAssembly());
 
-    // options.UseMySql(builder.Configuration.GetConnectionString("MySql"), ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("MySql")));
-    options.UseNpgsql(builder.Configuration.GetConnectionString("PostgreSQL"));
-    options.LogTo(Console.WriteLine, LogLevel.Information)
-                .EnableSensitiveDataLogging()
-                .EnableDetailedErrors();
-});
-builder.Services.AddUnitOfWork<ApplicationDbContext>();
-builder.Services.AddPostgreSqlTransactionHandler();
-builder.Services.AddCap(x =>
-{
-    x.UseEntityFramework<ApplicationDbContext>();
-    x.UseRabbitMQ(p => builder.Configuration.GetSection("RabbitMQ").Bind(p));
-});
-#endregion
-var app = builder.Build();
+    #endregion
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    #region Query
+
+    builder.Services.AddScoped<OrderQuery>();
+
+    #endregion
+
+
+    #region 基础设施
+
+    builder.Services.AddMediatR(cfg =>
+        cfg.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly()).AddUnitOfWorkBehaviors());
+    builder.Services.AddRepositories(typeof(ApplicationDbContext).Assembly);
+
+    builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    {
+        //options.UseInMemoryDatabase("ApplicationDbContext");
+
+        // options.UseMySql(builder.Configuration.GetConnectionString("MySql"), ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("MySql")));
+        options.UseNpgsql(builder.Configuration.GetConnectionString("PostgreSQL"));
+        options.LogTo(Console.WriteLine, LogLevel.Information)
+            .EnableSensitiveDataLogging()
+            .EnableDetailedErrors();
+    });
+    builder.Services.AddUnitOfWork<ApplicationDbContext>();
+    builder.Services.AddPostgreSqlTransactionHandler();
+    builder.Services.AddCap(x =>
+    {
+        x.UseEntityFramework<ApplicationDbContext>();
+        x.UseRabbitMQ(p => builder.Configuration.GetSection("RabbitMQ").Bind(p));
+    });
+
+    #endregion
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseStaticFiles();
+    app.UseHttpsRedirection();
+    app.UseRouting();
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    #region SignalR
+
+    app.MapHub<ABC.Template.Web.Application.Hubs.ChatHub>("/chat");
+
+    #endregion
+
+    app.UseHttpMetrics();
+    app.UseEndpoints(endpoints =>
+    {
+        endpoints.MapHealthChecks("/health");
+        endpoints.MapMetrics(); // 通过   /metrics  访问指标
+    });
+
+    app.Run();
 }
-app.UseStaticFiles();
-app.UseHttpsRedirection();
-app.UseRouting();
-app.UseAuthorization();
-
-app.MapControllers();
-
-#region SignalR
-app.MapHub<ABC.Template.Web.Application.Hubs.ChatHub>("/chat");
-#endregion
-app.UseHttpMetrics();
-app.UseEndpoints(endpoints =>
+catch (Exception ex)
 {
-    endpoints.MapHealthChecks("/health");
-    endpoints.MapMetrics();   // 通过   /metrics  访问指标
-});
+    Log.Fatal(ex, "Application terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
 
-app.Run();
-
-
-
-public partial class Program { }
+public partial class Program
+{
+}
