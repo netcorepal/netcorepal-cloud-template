f59efaf62ef0199916f9514019505ce1f0b3c6eb
witskeeper <witskeeper@gmail.com>
2024-07-29 23:29:00 +0800

    use NetCorePal 1.2.0 add httpclient add ResilienceHandler

---
+++ template/src/ABC.Template.Web/Clients/IUserServiceClient.cs
@@ -0,0 +1,11 @@
+using Refit;
+
+namespace ABC.Template.Web.Clients;
+
+public interface IUserServiceClient
+{
+    [Get("/users/{userId}")]
+    Task<UserDto> GetUserAsync(long userId);
+}
+
+public record UserDto(string Name, string Email, string Phone);
--- template/src/ABC.Template.Web/Controllers/OrderController.cs
+++ template/src/ABC.Template.Web/Controllers/OrderController.cs
@@ -8,41 +8,42 @@ using Microsoft.AspNetCore.Mvc;
 using NetCorePal.Extensions.DistributedTransactions.Sagas;
 using NetCorePal.Extensions.Domain;
 
-namespace ABC.Template.Web.Controllers
+namespace ABC.Template.Web.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class OrderController(IMediator mediator, OrderQuery orderQuery, ICapPublisher capPublisher) : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class OrderController(IMediator mediator, OrderQuery orderQuery, ICapPublisher capPublisher) : ControllerBase
+    [HttpGet]
+    public IActionResult Get()
     {
-        [HttpGet]
-        public IActionResult Get()
-        {
-            return Ok("Hello World");
-        }
-
-        [HttpPost]
-        public async Task<OrderId> Post([FromBody] CreateOrderCommand command)
-        {
-            var id = await mediator.Send(command);
-            return id;
-        }
-
-
-        [HttpGet]
-        [Route("/get/{id}")]
-        public async Task<ResponseData<Order?>> GetById([FromRoute] OrderId id)
-        {
-            var order = await orderQuery.QueryOrder(id, HttpContext.RequestAborted).AsResponseData();
-            return order;
-        }
-
-
-        [HttpGet]
-        [Route("/sendEvent")]
-        public async Task SendEvent(OrderId id)
-        {
-            await capPublisher.PublishAsync("OrderPaidIntegrationEvent", new OrderPaidIntegrationEvent(id));
-        }
+        return Ok("Hello World");
     }
-    
+
+    [HttpPost]
+    public async Task<ResponseData<OrderId>> Post([FromBody] CreateOrderRequest request)
+    {
+        var cmd = new CreateOrderCommand(request.Name, request.Price, request.Count);
+        var id = await mediator.Send(cmd);
+        return id.AsResponseData();
+    }
+
+
+    [HttpGet]
+    [Route("/get/{id}")]
+    public async Task<ResponseData<Order?>> GetById([FromRoute] OrderId id)
+    {
+        var order = await orderQuery.QueryOrder(id, HttpContext.RequestAborted).AsResponseData();
+        return order;
+    }
+
+
+    [HttpGet]
+    [Route("/sendEvent")]
+    public async Task SendEvent(OrderId id)
+    {
+        await capPublisher.PublishAsync("OrderPaidIntegrationEvent", new OrderPaidIntegrationEvent(id));
+    }
 }
+
+public record CreateOrderRequest(string Name, int Price, int Count);
