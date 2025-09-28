3ac50f50180efd95f3029ffcc4a22e60c0524928
witskeeper <witskeeper@gmail.com>
2025-09-28 10:11:07 +0800

    fix: 修复 Redis 配置绑定，确保连接字符串不为空

--- template/src/ABC.Template.Web/Program.cs
+++ template/src/ABC.Template.Web/Program.cs
@@ -160,7 +160,7 @@ try
 //#elif (UseNATS)
         x.UseNATS(p => builder.Configuration.GetSection("NATS").Bind(p));
 //#elif (UseRedisStreams)
-        x.UseRedis(builder.Configuration.GetConnectionString("Redis"));
+        x.UseRedis(builder.Configuration.GetConnectionString("Redis")!);
 //#elif (UsePulsar)
         x.UsePulsar(p => builder.Configuration.GetSection("Pulsar").Bind(p));
 //#endif
