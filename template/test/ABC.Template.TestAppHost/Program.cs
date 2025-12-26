e97d262b64f22a23aa042634b723112dbecc8ee8
witskeeper <witskeeper@gmail.com>
2025-12-26 17:20:11 +0800

    重构Program.cs文件，整理代码结构，确保数据库和消息队列基础设施的添加逻辑清晰可读

--- template/test/ABC.Template.TestAppHost/Program.cs
+++ template/test/ABC.Template.TestAppHost/Program.cs
@@ -1,48 +1,59 @@
-var builder = DistributedApplication.CreateBuilder(args);
+namespace ABC.Template.TestAppHost;
 
-// Add Redis infrastructure
-var redis = builder.AddRedis("Redis");
+public static class Program
+{
+    public static async Task Main(string[] args)
+    {
 
-//#if (!UseSqlite)
-var databasePassword = builder.AddParameter("database-password", value: "123456@Abc", secret: true);
-//#endif
-//#if (UseMySql)
-// Add MySQL database infrastructure
-var mysql = builder.AddMySql("Database", password: databasePassword);
+        var builder = DistributedApplication.CreateBuilder(args);
 
-var mysqlDb = mysql.AddDatabase("MySql", "test");
-//#elif (UseSqlServer)
-// Add SQL Server database infrastructure
-var sqlserver = builder.AddSqlServer("Database", password: databasePassword);
+        // Add Redis infrastructure
+        var redis = builder.AddRedis("Redis");
 
-var sqlserverDb = sqlserver.AddDatabase("SqlServer", "test");
-//#elif (UsePostgreSQL)
-// Add PostgreSQL database infrastructure
-var postgres = builder.AddPostgres("Database", password: databasePassword);
+        //#if (!UseSqlite)
+        var databasePassword = builder.AddParameter("database-password", value: "123456@Abc", secret: true);
+        //#endif
+        //#if (UseMySql)
+        // Add MySQL database infrastructure
+        var mysql = builder.AddMySql("Database", password: databasePassword);
 
-var postgresDb = postgres.AddDatabase("PostgreSQL", "test");
-//#elif (UseGaussDB)
-// Add GaussDB database infrastructure using OpenGauss container (GaussDB compatible)
-var gaussdb = builder.AddOpenGauss("Database", password: databasePassword);
+        var mysqlDb = mysql.AddDatabase("MySql", "test");
+        //#elif (UseSqlServer)
+        // Add SQL Server database infrastructure
+        var sqlserver = builder.AddSqlServer("Database", password: databasePassword);
 
-var gaussdbDb = gaussdb.AddDatabase("GaussDB", "test");
-//#elif (UseDMDB)
-// Add DMDB database infrastructure using DMDB container
-var dmdb = builder.AddDmdb("Database")
-    .WithDataVolume(isReadOnly: false);
+        var sqlserverDb = sqlserver.AddDatabase("SqlServer", "test");
+        //#elif (UsePostgreSQL)
+        // Add PostgreSQL database infrastructure
+        var postgres = builder.AddPostgres("Database", password: databasePassword);
 
-var dmdbDb = dmdb.AddDatabase("DMDB");
-//#endif
-//#if (UseSqlite)
-// SQLite is a file-based database and doesn't require container infrastructure
-//#endif
+        var postgresDb = postgres.AddDatabase("PostgreSQL", "test");
+        //#elif (UseGaussDB)
+        // Add GaussDB database infrastructure using OpenGauss container (GaussDB compatible)
+        var gaussdb = builder.AddOpenGauss("Database", password: databasePassword);
 
-//#if (UseRabbitMQ)
-// Add RabbitMQ message queue infrastructure
-var rabbitmq = builder.AddRabbitMQ("rabbitmq");
-//#elif (UseKafka)
-// Add Kafka message queue infrastructure
-var kafka = builder.AddKafka("kafka");
-//#endif
+        var gaussdbDb = gaussdb.AddDatabase("GaussDB", "test");
+        //#elif (UseDMDB)
+        // Add DMDB database infrastructure using DMDB container
+        var dmdb = builder.AddDmdb("Database")
+            .WithDataVolume(isReadOnly: false);
 
-await builder.Build().RunAsync();
+        var dmdbDb = dmdb.AddDatabase("DMDB");
+        //#endif
+        //#if (UseSqlite)
+        // SQLite is a file-based database and doesn't require container infrastructure
+        //#endif
+
+        //#if (UseRabbitMQ)
+        // Add RabbitMQ message queue infrastructure
+        var rabbitmq = builder.AddRabbitMQ("rabbitmq");
+        //#elif (UseKafka)
+        // Add Kafka message queue infrastructure
+        var kafka = builder.AddKafka("kafka");
+        //#endif
+
+        await builder.Build().RunAsync();
+
+    }
+}
+
